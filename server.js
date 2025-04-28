91605f4efc868b18576a12e4a274ab3820fa635c
Simen732 <Simenwearstad2@gmail.com>
2025-04-28 10:41:44 +0200

    endret port for å fikse VM

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.get("*", (req, res) => {
     res.render("error", {message: "Siden finnes ikke"});
 }) 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
